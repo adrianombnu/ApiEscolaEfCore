15ac15559ee5adfb3b3e62627fdf9d23e56ca12f
Adriano <adriano.marchi@premier.soft.net>
2022-01-10 09:49:53 -0300

    Ajustes

--- ApiEscola/Services/MateriaService.cs
+++ ApiEscola/Services/MateriaService.cs
@@ -14,18 +14,21 @@ namespace ApiEscolaEfCore.Services
         private readonly IMateriaRepositoryEfCore _iMateriaRepositoryEfCore;
         private readonly ProfessorRepository _professorRepository;
         private readonly IProfessorRepository _iProfessorRepository;
+        private readonly IUnitOfWork _unitOfWork;
 
         public MateriaService(MateriaRepository materiaRepository,
                               ProfessorRepository professorRepository,
                               IMateriaRepository iMateriaRepository,
                               IMateriaRepositoryEfCore iMateriaRepositoryEfCore,
-                              IProfessorRepository iProfessorRepository)
+                              IProfessorRepository iProfessorRepository,
+                              IUnitOfWork unitOfWork)
         {
             _materiaRepository = materiaRepository;
             _iMateriaRepository = iMateriaRepository;
             _iMateriaRepositoryEfCore = iMateriaRepositoryEfCore;
             _professorRepository = professorRepository;
             _iProfessorRepository = iProfessorRepository;
+            _unitOfWork = unitOfWork;
         }
 
         public ResultadoDTO Cadastrar(Materia materia)
@@ -43,8 +46,12 @@ namespace ApiEscolaEfCore.Services
             if (professor is null)
                 return ResultadoDTO.ErroResultado("O prefessor informado não foi encontrado!");
 
-            if (!_materiaRepository.Cadastrar(materia))
+            /*if (!_materiaRepository.Cadastrar(materia))
                 return ResultadoDTO.ErroResultado("Não foi possível cadastrar a matéria!");
+            */
+
+            _iMateriaRepositoryEfCore.Incluir(materia);
+            _unitOfWork.Commit();
 
             return ResultadoDTO.SucessoResultado(materia);
 
@@ -65,8 +72,12 @@ namespace ApiEscolaEfCore.Services
             if (professor is null)
                 return ResultadoDTO.ErroResultado("O prefessor informado não foi encontrado!");
 
-            if (!_materiaRepository.AtualizarMateria(materia))
-                return ResultadoDTO.ErroResultado("Matéria não pode ser atualizada!");
+            /*
+                 if (!_materiaRepository.AtualizarMateria(materia))
+                 return ResultadoDTO.ErroResultado("Matéria não pode ser atualizada!");
+            */
+            
+            _iMateriaRepositoryEfCore.Atualizar(materia);
 
             return ResultadoDTO.SucessoResultado(materia);
 
@@ -82,10 +93,14 @@ namespace ApiEscolaEfCore.Services
             if (_iMateriaRepository.VerificaSePossuiTurmaVinculada(id))
                 return ResultadoDTO.ErroResultado("Matéria já está vinculada a uma turma, favor remover o vinculo");
 
-            if (_materiaRepository.RomoverMateria(id))
+            /*if (_materiaRepository.RomoverMateria(id))
                 return ResultadoDTO.SucessoResultado();
             else
                 return ResultadoDTO.ErroResultado("Erro ao remover a matéria");
+            */
+            _iMateriaRepositoryEfCore.Remover(materia);
+
+            return ResultadoDTO.SucessoResultado();
 
         }
 
